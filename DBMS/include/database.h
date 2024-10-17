2ac5579c00fa3e51c25542e6bb39a646ddfbc9ba
Mikhail <161682783+fl1tzzy@users.noreply.github.com>
2024-10-17 18:47:41 +0700

    Update database.h

--- DBMS/include/database.h
+++ DBMS/include/database.h
@@ -5,26 +5,42 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Структура для хранения списка колонок
-typedef struct {
-    char **columns;
-    size_t count;
-} ColumnsList;
+typedef struct DataNode {
+    char *data;
+    struct DataNode *next;
+} DataNode;
 
-// Структура для хранения пары ключ-значение (таблица-колонки)
+typedef struct {
+    char *name;
+    DataNode *data;
+} Column;
+
 typedef struct {
     char *table_name;
-    ColumnsList *columns;
-} TableEntry;
+    Column *columns;
+    size_t column_count;
+    size_t row_count; 
+} Table;
 
-// Структура для хранения информации о схеме
 typedef struct {
     char *name;
     int tuples_limit;
-    TableEntry *tables;
+    Table *tables;
     size_t table_count;
 } DataBase;
 
-void free_database(DataBase* schema);
+DataBase* create_database(const char *database_name, DataBase *db);
+
+void add_table_to_database(DataBase *db, const char *table_name);
+void add_column_to_table(Table *table, const char *column_name);
+void add_data_to_table(Table *table, char **data);
+
+int get_column_index(Table *table, const char *column_name);
+
+void delete_row(Table *table, size_t row_index);
+
+void free_database(DataBase *schema);
+void free_column_data(Column *column);
+void free_table_data(Table *table);
 
 #endif
